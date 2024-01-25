29e2e362a312748cdbbdada62eac3144b6c54509
Jonatas Costa <jonathanevescosta@gmail.com>
2024-01-25 13:07:53 -0300

    Auth login implemented

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthRegisterDto } from './dto/auth-register.dto';
+import { AuthLoginDto } from './dto/auth-login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async regsiter(@Body()data: AuthRegisterDto){
+  async register(@Body()data: AuthRegisterDto){
     return this.authService.register(data);
   }
+
+  @Post('login')
+  async login(@Body(){email, password}: AuthLoginDto){
+    return this.authService.login(email, password);
+  }
+  
 }
